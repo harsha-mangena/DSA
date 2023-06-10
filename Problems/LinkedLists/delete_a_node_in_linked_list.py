16a6ecbbea9089e17ce0d1869b16d710f5054c89
harsha-mangena <harsha.mangena@gmail.com>
2023-06-10 16:59:20 +0530

    typo fix

--- Problems/LinkedLists/delete_a_node_in_linked_list.py
+++ Problems/LinkedLists/delete_a_node_in_linked_list.py
@@ -5,4 +5,4 @@ class Solution:
         :rtype: void Do not return anything, modify node in-place instead.
         """
         node.val = node.next.val
-        node.next = node.next.nex
+        node.next = node.next.next
